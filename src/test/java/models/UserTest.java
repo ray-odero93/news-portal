448a49accf4ef6c6741c38e84ed1fc3508997d49
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-24 11:28:09 +0300

    Create test to validate instantiation of user object.

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UserTest {
+
+    @Test
+    public void newUser_instantiatesCorrectly_true() {
+        assertTrue(true);
+    }
 
 }

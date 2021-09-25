c37c942443cc6d697720eb5786610ba75a30ed12
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-25 10:53:21 +0300

    Generate constructor function for department object.

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Department {
+    private int id;
+    private  String name;
+    private String description;
+
+    public Department(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }

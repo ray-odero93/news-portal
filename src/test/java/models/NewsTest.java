835a74803c2b3ea16f21330b8fc0f30942bf67e4
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-25 10:49:58 +0300

    Create tests to validate constructor function and getter & setter methods in News class.

---
+++ src/test/java/models/NewsTest.java
@@ -0,0 +1,26 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NewsTest {
+
+    @Test
+    public void newNews_instantiatesCorrectly_true() {
+        new News(1, "entertainment", "jay z new album.");
+        assertTrue(true);
+    }
+
+    @Test
+    public void getType_returnsCorrectNameAttribute_string() {
+        News news = new News(1, "entertainment", "jay z new album.");
+        assertEquals("entertainment", news.getType());
+    }
+
+    @Test
+    public void getContent_returnsCorrectContentAttribute_string() {
+        News news = new News(1, "entertainment", "jay z new album.");
+        assertEquals("jay z new album.", news.getContent());
+    }
+}

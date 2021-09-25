a34bffefca26a9d012d058ecb375ae494e574ee2
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-25 10:29:41 +0300

    Generate constructor function and getter & setter methods.

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,59 @@
 package models;
 
+import java.sql.Timestamp;
+
 public class News {
+    private int id;
+    private int userId;
+    private String type;
+    private  String content;
+    private Timestamp postDate;
+
+    public News(int id, int userId, String type, String content, Timestamp postDate) {
+        this.id = id;
+        this.userId = userId;
+        this.type = type;
+        this.content = content;
+        this.postDate = postDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Timestamp postDate) {
+        this.postDate = postDate;
+    }
 }

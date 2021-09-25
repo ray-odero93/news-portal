582f421fbd89baf79e30975153b150fd6eed58ec
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-25 10:57:26 +0300

    Create test file for news class with necessary test methods.

---
+++ src/test/java/models/DepartmentTest.java
@@ -0,0 +1,32 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepartmentTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+}

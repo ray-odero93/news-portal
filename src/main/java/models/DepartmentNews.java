8880a67ecf6b52ca4f74a7d88ebc1ad12847a7f7
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-26 15:07:53 +0300

    Generate constructor function, getter & setter methods and equals() & hashCode() methods.

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,4 +1,35 @@
 package models;
 
-public class DepartmentNews {
+import java.sql.Timestamp;
+import java.util.Objects;
+
+public class DepartmentNews extends News{
+    private int departmentId;
+
+    public DepartmentNews(int id, int userId, String type, String content, Timestamp postDate, int departmentId) {
+        super(id, userId, type, content, postDate);
+        this.departmentId = departmentId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DepartmentNews that = (DepartmentNews) o;
+        return departmentId == that.departmentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), departmentId);
+    }
 }

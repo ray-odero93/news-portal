374b7838a2184f65395942f4d35aac1be443a12b
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-26 11:01:14 +0300

    Add dao file for news class with methods for handling all database data on general news & department news; creating, reading, updating and deleting.

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,26 @@
 package dao;
 
+import com.sun.tools.javac.util.List;
+import jdk.nashorn.internal.runtime.ListAdapter;
+import models.DepartmentNews;
+import models.News;
+
 public interface NewsDao {
+//    create
+    void addGeneralNews(News news);
+    void addDepartmentNews(DepartmentNews departmentNews);
+
+//    read
+    List<News> getAllNews();
+    List<News> getGeneralNews();
+    List<DepartmentNews> getDepartmentNews();
+
+//    update
+    void updateGeneralNews(News news, int userId, String content);
+    void updateDepartmentNews(DepartmentNews departmentNews, int userId, String content, int departmentId);
+
+//    delete
+    void clearAllNews();
+    void clearAllGeneralNews();
+    void clearAllDepartmentNews();
 }

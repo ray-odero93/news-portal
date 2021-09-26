3f9017adc29bb3e49acaeb48995778b4f746fd20
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-26 10:54:43 +0300

    Add dao file for department class with methods for handling all database data on departments; creating, reading, updating and deleting.

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Department;
+import models.DepartmentNews;
+import models.User;
+
+import java.util.List;
+
 public interface DepartmentDao {
+//    create
+    void addDepartment(DepartmentDao department);
+
+//    read
+    List<Department> getAllDepartments();
+    List<User> getDepartmentUsersById(int id);
+    List<DepartmentNews> getDepartmentNewsById(int id);
+    Department findDepartmentById(int id);
+
+//    update
+    void updateDepartment(Department department, String name, String description);
+
+//    delete
+    void clearAllDepartments();
 }

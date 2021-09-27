d8eff62881b362116d97b7d399473441d0816715
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-27 05:01:08 +0300

    Add routes in the main app file.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,10 @@ import com.google.gson.Gson;
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oNewsDao;
 import dao.Sql2oUserDao;
+import models.Department;
+import models.DepartmentNews;
+import models.News;
+import models.User;
 import org.slf4j.LoggerFactory;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -55,5 +59,77 @@ public class App {
         newsDao = new Sql2oNewsDao(sql2o);
 
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            response.redirect("layout.hbs");
+            return null;
+        }
+        );
+
+        get("/users", (req,res)-> gson.toJson(userDao.getAllUsers()));
+        get("/departments", (req,res)-> gson.toJson(departmentDao.getDepartmentWithUserCount()));
+        get("/users/:id",(req,res)->{
+            int user_id = Integer.parseInt(req.params("id"));
+            return gson.toJson(userDao.findUserById(user_id));
+        });
+        get("/departments/:id",(req,res)->{
+            int dpt_id = Integer.parseInt(req.params("id"));
+            return gson.toJson(departmentDao.findDepartmentById(dpt_id));
+        });
+        get("/departments/:id/users",(req, res)->{
+            int dpt_id = Integer.parseInt(req.params("id"));
+            return gson.toJson(departmentId.getDepartmentUsersById(dpt_id));
+        });
+        get("/departments/:id/news",(req,res)->{
+            int dpt_id = Integer.parseInt(req.params("id"));
+            return gson.toJson(departmentId.getDepartmentNewsById(dpt_id));
+        });
+        get("/news", (req,res)-> gson.toJson(newsDao.getAllNews()));
+        get("/news/general", (req,res)-> gson.toJson(newsDao.getAllGeneralNews()));
+        get("/news/department", (req,res)-> gson.toJson(newsDao.getAllDepartmentNews()));
+
+        post("/Departments/new", "application/json", (req,res)->{
+            Department dpt = gson.fromJson(req.body(),Department.class);
+
+            departmentId.addDepartment(dpt);
+            res.status(201);
+            res.type("application/json");
+            res.redirect("/departments");
+            return gson.toJson(dpt);
+        });
+        post("/Users/new", "application/json", (req,res)->{
+            User user = gson.fromJson(req.body(), User.class);
+
+            userDao.addUser(user);
+            res.status(201);
+            res.type("application/json");
+
+            res.redirect("/users");
+            return gson.toJson(user);
+        });
+        post("/News/new", "application/json", (req,res)->{
+            News news = gson.fromJson(req.body(), News.class);
+
+            newsDao.addGeneralNews(news);
+            res.status(201);
+            res.type("application/json");
+            res.redirect("/news/general");
+            return gson.toJson(news);
+        });
+        post("/DepartmentNews/new", "application/json", (req,res)->{
+            DepartmentNews departmentNews = gson.fromJson(req.body(), DepartmentNews.class);
+
+            newsDao.addDepartmentNews(departmentNews);
+            res.status(201);
+            res.type("application/json");
+
+            res.redirect("/news/department");
+            return gson.toJson(departmentNews);
+        });
+
+        //FILTERS
+        after((req, res) ->{
+            res.type("application/json");
+        });
     }
 }
